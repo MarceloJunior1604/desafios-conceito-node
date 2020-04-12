e6ca044f2fca90232321ee103eafe06144045660
Marcelo Junior <marcelojunior1604@hotmail.com>
2020-04-11 21:14:07 -0300

    Desafio concluido

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const {uuid, isUuid} =  require('uuidv4');
 
 const app = express();
 
@@ -9,25 +8,68 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+// repositories : Variavel principal ,  repository : utilizarei nos arrey
+// findrepository : variavel para bsucar dentro do array
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
+ 
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url , techs} =  request.body;
+  const repository = { 
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes:0,
+  };
+  repositories.push(repository);
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs, likes} =  request.body;
+  const findrepositories =  repositories.findIndex(repository=> repository.id === id);
+  if (findrepositories === -1){
+    return response.status(400).json({error : "ID not exist or invalid"})
+  };
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[findrepositories].likes,
+
+  };
+   repositories[findrepositories] = repository;
+   return response.json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
-});
+app.delete("/repositories/:id", (request, response) => {
+  const { id } =  request.params;
+  const findrepositories =  repositories.findIndex(repository => repository.id === id);
+  if( findrepositories >= 0) {
+    repositories.splice(findrepositories,1);
+  }
+  else{
+    return response.status(400).json({erro : " ID not exist"})
+  }
+  return response.status(204).send()
+  });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { likes} =  request.body
+  const findrepositories =  repositories.findIndex(repository=> repository.id === id);
+  if (findrepositories < 0){
+    return response.status(400).json({error : "ID not exist or invalid"})
+  };
+  repositories[findrepositories].likes++;
+  return response.json(repositories[findrepositories]);
 });
 
 module.exports = app;
